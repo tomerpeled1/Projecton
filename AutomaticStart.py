c0340518e0be2fef3cafeb468822bbaca8382a6c
pitzpootz <shapira.nitzan@gmail.com>
2019-03-21 14:26:04 +0200

    Fixed typo in ANCOR_POINT

--- AutomaticStart.py
+++ AutomaticStart.py
@@ -3,14 +3,14 @@ import Algorithmics as Al
 import ArduinoCommunication as Ac
 import ImageProcessing as Ip
 
-ANCOR_POINT = 0, 0  # TODO write something imformative here
+ANCHOR_POINT = 0, 0  # TODO write something imformative here
 
 
 def automatic_start():
     # cut the apple for start
     arm_loc = Ac.start_cut()
 
-    Al.do_slice(St.slice_to_point(arm_loc, ANCOR_POINT))  # TODO write slice_to_point()
+    Al.do_slice(St.slice_to_point(arm_loc, ANCHOR_POINT))  # TODO write slice_to_point()
     if Ip.check_ad():
         Al.do_slice(St.ad_slice())  # TODO write ad_slice()
 
