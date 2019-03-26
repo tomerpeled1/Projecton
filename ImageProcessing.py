22ed54ff822689dec8787ad5d97cda8d8a68a750
eranmilo <40374196+eranmilo@users.noreply.github.com>
2019-03-27 00:31:20 +0200

    fixing the drawing problem - hopefully (deleted numbers and used magic nubers in crop_image in CameraInterface)

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -62,7 +62,7 @@ def draw_center(fruit, frame):
         cv2.circle(frame, cen[0][0:2], 2, (0, 0, 255), -1)
 
 
-def draw_trajectory(fruit, frame):
+def draw_trajectory(fruit, frame, time_of_frame):
     """
     draws the predicted route of the fruit  and the centers we found for it
     on the specific frame. used mostly for debugging.
@@ -78,10 +78,19 @@ def draw_trajectory(fruit, frame):
         # size = frame.shape
         xt, yt = xy_by_t(t)
         yt_pixel, xt_pixel, _ = Algo.cm2pixel((xt, yt, t))
-        cv2.circle(frame, (xt_pixel, yt_pixel), 3, (0, int(85*t), 255), -1)
+        cv2.circle(frame, (xt_pixel, yt_pixel), 3, (0, int(85 * t), 255), -1)
+
+        # if t in [0.8]:
+        #     cv2.circle(frame, (xt_pixel, yt_pixel), 5, (255, 0, 255), -1)
     centers = [Algo.crop2frame(center[0]) for center in fruit.centers]
     for center in centers:
         cv2.circle(frame, (center[0], center[1]), 3, (0, 255, 0), -1)
+
+    # draw the prediction real time
+    x,y = xy_by_t(time_of_frame - fruit.time_created)
+    y_pix, x_pix ,_=  Algo.cm2pixel((x, y, time_of_frame - fruit.time_created))
+    cv2.circle(frame, (x_pix, y_pix), 6, (60, 255, 0), -1)
+
 
     # centers = [center[0] for center in fruit.centers]
     # x_coords = [center[0] for center in centers]
@@ -198,7 +207,9 @@ def update_trajectories(fruits_to_extract):
             try:
                 fruit.trajectory = Algo.get_trajectory_by_fruit_locations(centers_cm)
                 fruits_for_debug_trajectories.append(fruit)
+                print("added")
             except Exception:
+                print("exception")
                 continue
 
 
@@ -274,7 +285,7 @@ def insert_new_fruits(detection_results, fruits_info, current):
     fruits_info += get_fruits_info(detection_results, current)
 
 
-def debug_with_buffer(buffer):
+def debug_with_buffer(buffer, buffer_of_shaninja):
     """
     Debugging method which shows images and allows you to pass easily between them.
     :param buffer: List of frames.
@@ -283,8 +294,13 @@ def debug_with_buffer(buffer):
     while True:
         for fruit in fruits_for_debug_trajectories:
             # draw_center(fruit, buffer[i])
-            draw_trajectory(fruit, buffer[i])
-        cv2.imshow("debug", buffer[i])
+            draw_trajectory(fruit, buffer[i][0],time_of_frame=0)
+            x, y = fruit.trajectory.calc_trajectory()(buffer[i][1] - fruit.time_created)
+            y_pix, x_pix, _ = Algo.cm2pixel((x, y, 0))
+            cv2.circle(buffer[i][0], (x_pix, y_pix), 6, (188, 0, 255), -1)
+
+        cv2.imshow("debug", buffer[i][0])
+        cv2.imshow("Shaninja", buffer_of_shaninja[i])
         x = cv2.waitKey(1)
         if x == 49:  # '1' key
             i -= 1
@@ -292,12 +308,12 @@ def debug_with_buffer(buffer):
             i += 1
 
 
-def show_original(camera):
+def show_original(camera,buffer_of_shaninja):
     """
     Show original frames taken by camera.
     :param camera: Camera object of camera
     """
-    debug_with_buffer(camera.buffer)
+    debug_with_buffer(camera.buffer, buffer_of_shaninja)
 
 
 def draw(fruit, frame):
